6a8a9aba1a94a9c9e949c35af4234a40d5d7b530
Ingrzs <ing.ramirozs@gmail.com>
2022-05-01 13:52:26 -0500

    create a card en trello from script

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card RZS ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "626ea85a9cbcf2122fc6edf4",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
